73ea71edba09d42cf35b13dcae257678ace603fb
Alberto Buzón Rodríguez <alberto.s378405@cesurformacion.com>
2025-10-07 13:03:57 +0200

    ProcessBuilder

---
+++ src/ejemplos/ProcessBuilderEjemplo.java
@@ -0,0 +1,17 @@
+package ejemplos;
+
+import java.io.*;
+
+public class ProcessBuilderEjemplo {
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            String url = "https://www.youtube.com";
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "start", url);
+            Process proceso = pb.start();
+            int exitcode = proceso.waitFor();
+            System.out.println("El proceso terminó con código "+exitcode);
+        } catch (IOException e) {
+            System.out.println("Error al ejecutar el proceso "+e.getMessage());
+        }
+    }
+}
