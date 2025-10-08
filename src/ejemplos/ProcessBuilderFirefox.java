dff2189f7f5108f088de63a0d5c6f8e72196decd
Alberto Buzón Rodríguez <alberto.s378405@cesurformacion.com>
2025-10-08 09:50:14 +0200

    Firefox mejorado

--- src/ejemplos/ProcessBuilderFirefox.java
+++ src/ejemplos/ProcessBuilderFirefox.java
@@ -5,8 +5,9 @@ import java.io.*;
 public class ProcessBuilderFirefox {
      public static void main(String[] args) throws InterruptedException {
         try {
-            String url = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-            ProcessBuilder pb = new ProcessBuilder(url);
+            String dir = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+            String urlweb = "https://www.youtube.com";
+            ProcessBuilder pb = new ProcessBuilder(dir, urlweb);
             Process proceso = pb.start();
             int exitcode = proceso.waitFor();
             System.out.println("El proceso terminó con código: "+ exitcode);
